ca56848b57659bf8b020ef0b8402d8a0f81ca366
mrinal kumar <mrinal.ts@gmail.com>
2019-11-16 01:08:21 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,4 @@
+# Changing the tuples
+index_tuple = (4, 2, 3, [6, 5, 7])  # The index of tuple carries numbers, where [6,5,7] are mutable indexes which can be changed.
+index_tuple[3] [2] = 10    # Here in index 3 , the value at 3rd index will be changed,
+print(index_tuple)
